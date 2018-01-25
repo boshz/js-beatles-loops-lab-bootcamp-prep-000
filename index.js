00ae152bac0cef14be62b05ff52cbe9d0015d680
JOSHUA ZYNDORF <zyndorf.1@osu.edu>
2018-01-25 15:18:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function johnLennonFacts(facts) {
   } 
   return n
 }
+
+function iLoveTheBeatles(num) {
+   var phrases = [];
+   do {
+     phrases.push("I love the Beatles!");
+     num++
+   } while (num < 15);
+   return phrases;
+}
